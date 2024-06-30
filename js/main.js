2499260718d712e08ec96e20075f49414dd0853f
Sebastian Salas <monsterxd_799@hotmail.com>
2024-06-30 20:19:32 -0300

    Haciendo pequeñas correcciónes en los textos y agregando un <break> que faltaba en el case 4

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 function mostrarMenu (){
     alert (' ---- MENU DE OPCIONES ----' + 
         '\n 1. Ingresar NUEVO empleado.' +
-        '\n 2. Cargar Asistencia.' +
-        '\n 3. Calcular el sueldo.' +
-        '\n 4. Agregar alguna bonificacion. ' +
+        '\n 2. Cargar ASISTENCIA.' +
+        '\n 3. Calcular el SUELDO.' +
+        '\n 4. Agregar BONIFICACION. ' +
         '\n 0. Salir.' 
     );
 }
@@ -22,7 +22,7 @@ function calcularHorasTrabajadas(horaInicio, horaFin) {
     if (horaInicio >= 0 && horaInicio <= 24 && horaFin >= 0 && horaFin <= 24 && horaFin >= horaInicio) {
         return horaFin - horaInicio;
     } else {
-        alert('Horas ingresadas no válidas. Intente nuevamente.');
+        alert('Horas ingresadas <NO VALIDAS>. Intente nuevamente.');
         return -1; // VALOR QUE INDICA ERROR
     }
 }
@@ -41,17 +41,17 @@ let sueldoTotal = 0;
 
 while (opcion !== 0) {
     mostrarMenu();
-    opcion = Number(prompt('ingrese una opcion: '));
+    opcion = Number(prompt('Ingrese una opcion del 0 al 4: '));
     switch(opcion){
         // INGRESAR EL NOMBRE DEL EMPLEADO.
         case 1 :
-            empleado = prompt('Ingrese el nombre del empleado: ');
+            empleado = prompt('Ingrese el NOMBRE del empleado: ');
             break;
 
         // CARGAR ASISTENCIA.
         case 2 :
-            let horasInicio = Number(prompt('Ingrese la hora de inicio (FROMATO 0 - 24hs): '));
-            let horasFin = Number(prompt('Ingrese la hora de fin (FROMATO 0 - 24hs): '));
+            let horasInicio = Number(prompt('Ingrese la HORA DE INGRESO (FROMATO 0 - 24hs): '));
+            let horasFin = Number(prompt('Ingrese la HORA DE EGRESO (FROMATO 0 - 24hs): '));
             horasTrabajadas = calcularHorasTrabajadas(horasInicio, horasFin);
             if (horasTrabajadas === -1) {
                 // SI NO ES VALIDO, NO CONTINUA. 
@@ -59,9 +59,9 @@ while (opcion !== 0) {
             }
             break;
 
-        // AGREGAR BONIFICACION.
+        // SACAR CALCULO DEL SUELDO.
         case 3 :
-            valorHora = Number(prompt('Ingrese el valor de la hora: '));
+            valorHora = Number(prompt('Ingrese el VALOR de la hora: '));
             let horasConExtra = esHoraExtra(horasTrabajadas);
             sueldoTotal = calculoHora(horasConExtra, valorHora);
             alert('EMPLEADO: '+ empleado + ' TOTAL: $' + sueldoTotal);
@@ -69,16 +69,18 @@ while (opcion !== 0) {
         
         // AGREGAR BONIFICACION EXTRA AL SUELDO.
         case 4 :
-            let bonificacion = Number(prompt('Ingresar BONIFICACION: $ '));
+            let bonificacion = Number(prompt('Ingrese el monto de la BONIFICACION: $ '));
             let sueldoConBoni = sueldoTotal + bonificacion;
             alert('EMPLEADO: '+ empleado + ' TOTAL: $' + sueldoConBoni);
+            break;
 
         //  SALIR DEL MENU
         case 0 :
             alert('Gracias por usar el sistema.');
             break;
+
         // EN CUALQUIER OTRA OPCION, MOSTRAR ESTE MENSAJE.
         default:
-            alert("Opcion NO VALIDA, vuelva a intentar. ");
+            alert("Opcion NO VALIDA, vuelva a intentar, por favor. ");
     }
 }
