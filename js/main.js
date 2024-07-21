8f28faa39bf87b100801099281840c17c058983c
Sebastian Salas <monsterxd_799@hotmail.com>
2024-07-21 01:25:44 -0300

    PreEntrega 2 - Agregando funciones de Orden superior, arrays, objetos vistos en clase a nuestro proyecto.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Autor: Salas, Sebastián Ariel. 
 // Comisión: 57730.
 
-// --- FUNCIONES ---
+// ----------------------- FUNCIONES ---------------------------------
 function mostrarMenu (){
     alert (' ---- MENU DE OPCIONES ----' + 
         '\n 1. Ingresar NUEVO empleado.' +
@@ -10,6 +10,23 @@ function mostrarMenu (){
         '\n 4. Agregar BONIFICACION. ' +
         '\n 0. Salir.' 
     );
+}
+// ---- CONSTRUCTOR DE NUEVOS EMPLEADOS -----
+function Empleado (nombre, apellido, dni){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.dni = dni;
+    this.horasTrabajadas = 0;
+    this.sueldoTotal = 0;
+
+}
+
+// --- PEDIR QUE EL EMPLEADO CARGUE SUS DATOS ----
+function obtenerDatosDeEmpleado(){
+    const nombre = prompt("Ingrese su NOMBRE: ");
+    const apellido = prompt("Ingrese su APELLIDO: ");
+    const dni = Number(prompt("Ingrese su DNI: "));
+    return new Empleado(nombre, apellido, dni);
 }
 
 function esHoraExtra(horasTrabajadas){
@@ -22,63 +39,100 @@ function calculoHora(horasTrabajadas, valorHora){
 
 // ESTA FUNCION CONTROLA QUE LA HORA FINAL NO SEA ANTERIOR A LA DE INICIO <FORMATO 0 - 24HS>.
 function calcularHorasTrabajadas(horaInicio, horaFin) {
-    if (horaInicio >= 0 && horaInicio <= 24 && horaFin >= 0 && horaFin <= 24 && horaFin >= horaInicio) {
+    if (horaInicio>=0 && horaFin<=24 && horaFin>horaInicio) {
         return horaFin - horaInicio;
     } else {
         alert('Horas ingresadas <NO VALIDAS>. Intente nuevamente.');
-        return -1; // VALOR QUE INDICA ERROR
+        return -1; // VALOR QUE INDICA ERROR    
     }
+}
+
+// FUNCION PARA SELECCIONAR EL EMPLEADO Y RETORNAR EL INDICE
+function seleccionarEmpleado (){
+    let empleadoSeleccionado = prompt("¿A que empleado desea seleccionar?")
+    return empleados.findIndex(empleado => empleado.nombre.toLowerCase() === empleadoSeleccionado.toLowerCase());
+}
+
+// FUNCION QUE CARGA LAS HORAS TRABAJADAS DE LOS EMPLEADOS
+function cargarHoras(indiceEmpleado) {
+    const horasInicio = Number(prompt("Ingrese la HORA DE INICIO"));
+    const horasFin = Number(prompt("Ingrese la HORA DE SALIDA"));
+    const horasTrabajadas = calcularHorasTrabajadas(horasInicio, horasFin);
+    if (horasTrabajadas !== -1) {
+        empleados[indiceEmpleado].horasTrabajadas += horasTrabajadas;
+    }
+    return horasTrabajadas;
 }
 
 alert("BIENVENIDO AL SISTEMA DE ASISTENCIA");
 
 // --- VARIABLES ---
 let opcion = -1;
-let empleado = '';
-let horasTrabajadas = 0;
-let valorHora = 0;
-let sueldoTotal = 0;
 
-// --- MAIN ---
-// MOSTRAR MENU DE OPCIONES
+// --- ARRAY DE EMPLEADOS ----
+const empleados = [];
+
+// ------------------------------------------------ MAIN ------------------------------------------------------
 
 while (opcion !== 0) {
-    mostrarMenu();
+    mostrarMenu(); // MOSTRAR MENU DE OPCIONES
     opcion = Number(prompt('Ingrese una opcion del 0 al 4: '));
     switch(opcion){
-        // INGRESAR EL NOMBRE DEL EMPLEADO.
+        // ----- CARGAR EMPLEADOS ---
         case 1 :
-            empleado = prompt('Ingrese el NOMBRE del empleado: ');
+            // ---- CARGA DE EMPLEADOS ----
+            let continuar = true;
+            while (continuar) {
+                const empleado = obtenerDatosDeEmpleado();
+                empleados.push(empleado);
+                continuar = confirm("¿Desea cargar otro empleado? ")
+                console.log(empleados);
+            }
             break;
 
         // CARGAR ASISTENCIA.
         case 2 :
-            let horasInicio = Number(prompt('Ingrese la HORA DE INGRESO (FROMATO 0 - 24hs): '));
-            let horasFin = Number(prompt('Ingrese la HORA DE EGRESO (FROMATO 0 - 24hs): '));
-            horasTrabajadas = calcularHorasTrabajadas(horasInicio, horasFin);
-            if (horasTrabajadas === -1) {
-                // SI NO ES VALIDO, NO CONTINUA. 
-                break;
+            // --- INSERTAR ASISTENCIA ----
+            let continua = true;
+            while (continua) {
+                let indiceEmpleado = seleccionarEmpleado();
+                if (indiceEmpleado !== -1 ){
+                    cargarHoras(indiceEmpleado);
+                    continua = confirm("¿Desea continuar cargando?")
+                }else{
+                    alert("El Empleado que intenta buscar, NO EXISTE. Por favor intente de nuevo.");
+                }
             }
             break;
 
         // SACAR CALCULO DEL SUELDO.
         case 3 :
-            valorHora = Number(prompt('Ingrese el VALOR de la hora: '));
-            let horasConExtra = esHoraExtra(horasTrabajadas);
-            sueldoTotal = calculoHora(horasConExtra, valorHora);
-            alert('EMPLEADO: '+ empleado + ' TOTAL: $' + sueldoTotal);
+            indiceEmpleadoSueldo = seleccionarEmpleado();
+            if (indiceEmpleadoSueldo !== -1){
+                valorHora = Number(prompt('Ingrese el VALOR de la hora: '));
+                let horasConExtra = esHoraExtra(empleados[indiceEmpleadoSueldo].horasTrabajadas);
+                empleados[indiceEmpleadoSueldo].sueldoTotal = calculoHora(horasConExtra, valorHora);
+                alert('EMPLEADO: '+ empleados[indiceEmpleadoSueldo].nombre.toUpperCase()+ ' TOTAL: $' + empleados[indiceEmpleadoSueldo].sueldoTotal);
+            }else{
+                alert("El Empleado que intenta buscar, NO EXISTE. Por favor intente de nuevo.");
+            }
             break;
         
         // AGREGAR BONIFICACION EXTRA AL SUELDO.
         case 4 :
-            let bonificacion = Number(prompt('Ingrese el monto de la BONIFICACION: $ '));
-            let sueldoConBoni = sueldoTotal + bonificacion;
-            alert('EMPLEADO: '+ empleado + ' TOTAL: $' + sueldoConBoni);
+            indiceEmpleadoBonificacion = seleccionarEmpleado();
+            if (indiceEmpleadoBonificacion !== -1){
+                let bonificacion = Number(prompt('Ingrese el monto de la BONIFICACION: $ '));
+                empleados[indiceEmpleadoBonificacion].sueldoTotal = empleados[indiceEmpleadoBonificacion].sueldoTotal + bonificacion;
+                alert('EMPLEADO: '+ empleados[indiceEmpleadoBonificacion].nombre.toUpperCase() + ' TOTAL: $' + empleados[indiceEmpleadoBonificacion].sueldoTotal);
+            }
+            else{
+                    alert("El Empleado que intenta buscar, NO EXISTE. Por favor intente de nuevo.");
+            }
             break;
 
         //  SALIR DEL MENU
-        case 0 :
+        case 0 :   
             alert('Gracias por usar el sistema.');
             break;
 
